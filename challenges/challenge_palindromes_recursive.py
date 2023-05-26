da3d96042d35c4b6008317d04197f8a795c58117
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-05-26 10:37:40 -0300

    feat: palimdromo ou não

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) <= 0 or type(word) != str:
+        return False
+
+    if low_index == high_index:
+        inverted_word = word[::-1]
+        if inverted_word == word:
+            return True
+        else:
+            return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index)
