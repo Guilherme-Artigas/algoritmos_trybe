cb4dd1452901b85cdf1366f309c624cc925f3ac7
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-05-26 07:48:59 -0300

    feat: algoritmo busca força bruta

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
+def validate_entries(list, target):
+    for i in list:
+        if type(i[0]) != int or i[0] < 0 or type(i[1]) != int or i[1] < 0:
+            return True
+
+    if type(target) != int or target < 0:
+        return True
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""  # OK
-    raise NotImplementedError
+    if validate_entries(permanence_period, target_time):
+        return None
+
+    contador = 0
+
+    for i in permanence_period:
+        if i[0] <= target_time and target_time <= i[1]:
+            contador += 1
+
+    return contador
