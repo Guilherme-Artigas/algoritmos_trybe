757152cc913694c3c2f8eac2ceb1afd992721d78
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-05-26 09:28:46 -0300

    test: funcionamento da função de criptografia

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+
+    with pytest.raises(TypeError):
+        encrypt_message("Guilherme", "a")
+
+    assert_one = encrypt_message("Guilherme", 15)
+    assert assert_one == "emrehliuG"
+
+    assert_two = encrypt_message("Guilherme", 4)
+    assert assert_two == "emreh_liuG"
+
+    assert_three = encrypt_message("Guilherme", 5)
+    assert assert_three == "hliuG_emre"
